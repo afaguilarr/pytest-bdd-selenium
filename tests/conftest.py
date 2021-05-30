0e73db9d079201446c363292b4e2696dd8ea1319
Los Payitos <los_payitos@Loss-MacBook-Pro.local>
2021-05-30 10:22:37 -0500

    Travis should really work now

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,23 @@ from typing import Dict
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+# Gets the headless command line argument
+# execute 'pytest --headless headless' to run headless
+def pytest_addoption(parser):
+    parser.addoption("--headless", action="store", default="")
+
+
 @pytest.fixture(scope="function")
-def web_driver() -> WebDriver:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def web_driver(pytestconfig) -> WebDriver:
+    chrome_options = Options()
+    if pytestconfig.getoption("headless"):
+        chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     yield driver
     driver.close()
     driver.quit()
